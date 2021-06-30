d850c587f5ca9e45c5bba65fc0426d1cac993674
Daniel_Ruiz <daniel.ruiz@chironix.com>
2021-06-30 23:33:40 +0800

    dummy publisher now works

--- Arduino_sketches/Dummy_publisher/Dummy_publisher.ino
+++ Arduino_sketches/Dummy_publisher/Dummy_publisher.ino
@@ -1,39 +1,35 @@
 #include <Arduino.h>
 #include <ros.h>
-#include <rosserial_arduino/Adc.h>
+#include <rpi_logger/voltages.h>
 
 ros::NodeHandle nh;
 
-rosserial_arduino::Adc adc_msg;
-ros::Publisher p("adc", &adc_msg);
+rpi_logger::voltages voltages_msg;
+ros::Publisher p("voltages", &voltages_msg);
 
 void setup()
 { 
   pinMode(13, OUTPUT);
   nh.initNode();
-
   nh.advertise(p);
 }
 
-//We average the analog reading to elminate some of the noise
-int averageAnalog(int pin){
-  int v=0;
-  for(int i=0; i<4; i++) v+= analogRead(pin);
-  return v/4;
-}
-
-long adc_timer;
 
 void loop()
-{
-  adc_msg.adc0 = averageAnalog(0);
-  adc_msg.adc1 = averageAnalog(1);
-  adc_msg.adc2 = averageAnalog(2);
-  adc_msg.adc3 = averageAnalog(3);
-  adc_msg.adc4 = averageAnalog(4);
-  adc_msg.adc5 = averageAnalog(5);
-    
-  p.publish(&adc_msg);
+{ 
+  voltages_msg.header.stamp = nh.now();
+  voltages_msg.V1=800;
+  voltages_msg.V2=600;
+  
+  p.publish(&voltages_msg);
 
+  delay(500);
+  voltages_msg.header.stamp = nh.now();
+  voltages_msg.V1=450;
+  voltages_msg.V2=250;
+
+  p.publish(&voltages_msg);
+
+  delay(500);
   nh.spinOnce();
 }
